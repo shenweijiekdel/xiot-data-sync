353ebfeaa37bfc6b3a48b3adf4232f540f86c933
沈伟杰 <shenweijie@knowin.com>
2020-12-07 15:38:42 +0800

    fix differ

--- app/src/main/java/cn/geekcity/xiot/service/TemplateDiffer.java
+++ app/src/main/java/cn/geekcity/xiot/service/TemplateDiffer.java
@@ -21,25 +21,29 @@ import java.util.stream.Collectors;
 
 public class TemplateDiffer implements DataDiffer<DeviceTemplate> {
 
-    private static void deviceDiff(DeviceTemplate source, DeviceTemplate target, List<String> messages) {
-        if (target == null) {
+    private static void deviceDiff(DeviceTemplate source, DeviceTemplate current, List<String> messages) {
+        if (current == null) {
             messages.add("当前环境不存在");
             return ;
         }
 
-        if (!source.type().toString().equals(target.type().toString())) {
+        if (!source.type().toString().equals(current.type().toString())) {
             messages.add("当前环境实例type不相同 ");
             return;
         }
 
-        descriptionDiff(source.description(), target.description(), messages);
-        servicesDiff(source.services(), target.services(), messages);
+        descriptionDiff(source.description(), current.description(), messages);
+        servicesDiff(source.services(), current.services(), messages);
     }
 
-    private static void servicesDiff(List<ServiceTemplate> source, List<ServiceTemplate> target, List<String> messages) {
+    private static void servicesDiff(List<ServiceTemplate> source, List<ServiceTemplate> current, List<String> messages) {
+        if (source.size() != current.size()) {
+            messages.add("service 个数不相同");
+        }
+
         for (ServiceTemplate sourceService : source) {
 
-            Optional<ServiceTemplate> first = target.stream().filter(x -> x.iid() == sourceService.iid()).findFirst();
+            Optional<ServiceTemplate> first = current.stream().filter(x -> x.iid() == sourceService.iid()).findFirst();
             if (!first.isPresent()) {
                 messages.add("service iid " + sourceService.iid() + "不存在");
                 continue;
@@ -49,20 +53,24 @@ public class TemplateDiffer implements DataDiffer<DeviceTemplate> {
         }
     }
 
-    private static void serviceDiff(ServiceTemplate source, ServiceTemplate target, List<String> messages) {
-        if (!source.type().toString().equals(target.type().toString())) {
+    private static void serviceDiff(ServiceTemplate source, ServiceTemplate current, List<String> messages) {
+        if (!source.type().toString().equals(current.type().toString())) {
             messages.add("service " + source.type().toString() + "存在差异");
             return;
         }
 
-        propertiesDiff(source.properties(), target.properties(), messages);
-        actionsDiff(source.actions(), target.actions(), messages);
-        eventsDiff(source.events(), target.events(), messages);
+        propertiesDiff(source.properties(), current.properties(), messages);
+        actionsDiff(source.actions(), current.actions(), messages);
+        eventsDiff(source.events(), current.events(), messages);
     }
 
-    private static void eventsDiff(List<EventTemplate> source, List<EventTemplate> target, List<String> messages) {
+    private static void eventsDiff(List<EventTemplate> source, List<EventTemplate> current, List<String> messages) {
+        if (source.size() != current.size()) {
+            messages.add("event 个数不相同");
+        }
+
         for (EventTemplate sourceEvent : source) {
-            Optional<EventTemplate> first = target.stream().filter(x -> x.iid() == sourceEvent.iid()).findFirst();
+            Optional<EventTemplate> first = current.stream().filter(x -> x.iid() == sourceEvent.iid()).findFirst();
             if (!first.isPresent()) {
                 messages.add("action " + sourceEvent.type().toString() + "不存在");
                 continue;
@@ -72,9 +80,13 @@ public class TemplateDiffer implements DataDiffer<DeviceTemplate> {
         }
     }
 
-    private static void actionsDiff(List<ActionTemplate> source, List<ActionTemplate> target, List<String> messages) {
+    private static void actionsDiff(List<ActionTemplate> source, List<ActionTemplate> current, List<String> messages) {
+        if (source.size() != current.size()) {
+            messages.add("action 个数不相同");
+        }
+
         for (ActionTemplate sourceAction : source) {
-            Optional<ActionTemplate> first = target.stream().filter(x -> x.iid() == sourceAction.iid()).findFirst();
+            Optional<ActionTemplate> first = current.stream().filter(x -> x.iid() == sourceAction.iid()).findFirst();
             if (!first.isPresent()) {
                 messages.add("action " + sourceAction.type().toString() + "不存在");
                 continue;
@@ -84,9 +96,13 @@ public class TemplateDiffer implements DataDiffer<DeviceTemplate> {
         }
     }
 
-    private static void propertiesDiff(List<PropertyTemplate> source, List<PropertyTemplate> target, List<String> messages) {
+    private static void propertiesDiff(List<PropertyTemplate> source, List<PropertyTemplate> current, List<String> messages) {
+        if (source.size() != current.size()) {
+            messages.add("property 个数不相同");
+        }
+
         for (PropertyTemplate sourceProperty : source) {
-            Optional<PropertyTemplate> first = target.stream().filter(x -> x.iid() == sourceProperty.iid()).findFirst();
+            Optional<PropertyTemplate> first = current.stream().filter(x -> x.iid() == sourceProperty.iid()).findFirst();
             if (!first.isPresent()) {
                 messages.add("property " + sourceProperty.type().toString() + "不存在");
                 continue;
@@ -96,33 +112,33 @@ public class TemplateDiffer implements DataDiffer<DeviceTemplate> {
         }
     }
 
-    private static void eventDiff(EventTemplate source, EventTemplate target, List<String> messages) {
+    private static void eventDiff(EventTemplate source, EventTemplate current, List<String> messages) {
         String sourceString = EventTemplateCodec.encode(source).encode();
-        String targetString = EventTemplateCodec.encode(target).encode();
-        if (!sourceString.equals(targetString)) {
+        String currentString = EventTemplateCodec.encode(current).encode();
+        if (!sourceString.equals(currentString)) {
             messages.add("event " + source.type().toString() + " 不同");
         }
     }
 
-    private static void actionDiff(ActionTemplate source, ActionTemplate target, List<String> messages) {
+    private static void actionDiff(ActionTemplate source, ActionTemplate current, List<String> messages) {
         String sourceString = ActionTemplateCodec.encode(source).encode();
-        String targetString = ActionTemplateCodec.encode(target).encode();
-        if (!sourceString.equals(targetString)) {
+        String currentString = ActionTemplateCodec.encode(current).encode();
+        if (!sourceString.equals(currentString)) {
             messages.add("action " + source.type().toString() + " 不同");
         }
     }
 
-    private static void propertyDiff(PropertyTemplate source, PropertyTemplate target, List<String> messages) {
+    private static void propertyDiff(PropertyTemplate source, PropertyTemplate current, List<String> messages) {
         String sourceString = PropertyTemplateCodec.encode(source).encode();
-        String targetString = PropertyTemplateCodec.encode(target).encode();
-        if (!sourceString.equals(targetString)) {
+        String currentString = PropertyTemplateCodec.encode(current).encode();
+        if (!sourceString.equals(currentString)) {
             messages.add("property " + source.type().toString() + " 不同");
         }
     }
 
-    private static void descriptionDiff(Map<String, String> source, Map<String, String> target, List<String> messages) {
+    private static void descriptionDiff(Map<String, String> source, Map<String, String> current, List<String> messages) {
         for (Map.Entry<String, String> s : source.entrySet()) {
-            String t = target.get(s.getKey());
+            String t = current.get(s.getKey());
 
             if (!t.equals(s.getValue())) {
                 messages.add("实例定义描述不同");
